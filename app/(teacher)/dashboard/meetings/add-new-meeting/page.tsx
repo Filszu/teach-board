9cb4249bb162700e4bc21b1020559e5049e74244
Filszu <filipszumowski05@gmail.com>
2023-06-02 21:17:38 +0200

    menu icon styled

---
+++ app/(teacher)/dashboard/meetings/add-new-meeting/page.tsx
@@ -0,0 +1,15 @@
+
+interface Props {
+}
+
+
+const Page = async(props: Props) => {
+    return (
+        <>
+            
+            <h1>Add new meeting</h1>
+        </>
+    )
+}
+
+export default Page
--- components/navbar/Navabar.tsx
+++ components/navbar/Navabar.tsx
@@ -2,6 +2,8 @@
 import UserIcon from "../userIcon/UserIcon"
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import styles from './Navbar.module.css'
+import { IconButton } from "@mui/material";
+import Link from "next/link";
 type Props = {}
 
 const Navabar = (props: Props) => {
@@ -10,7 +12,15 @@ const Navabar = (props: Props) => {
         <div>
          <h2>Teacher Dashboard</h2>
         </div>
-        <div className={styles.menuLeft}><AddCircleIcon/></div>
+        <div className={styles.menuLeft}>
+          <Link href="./dashboard/meetings/add-new-meeting">
+          <span className={styles.nav__navIcon}>
+          <IconButton aria-label="add new meeting" color="inherit" >
+            <AddCircleIcon  sx={{ fontSize: 30 }}/>
+          </IconButton>
+          </span>
+          </Link>
+        </div>
         <div>
           <UserIcon userName='Filszu' />
         </div>
